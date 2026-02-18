bf67e7962d389eb4eae285d5ef4ad709f761c8de
incredet <dwgira@gmail.com>
2026-02-18 16:39:14 +0200

    gtrefmwod

--- documentation/__init__.py
+++ documentation/__init__.py
@@ -2,23 +2,23 @@
 # REDO THE __INIT__ AND CONFIG FILES
 # ==========================================================================
 
-from .pairs_artifacts import (
-    utc_now_iso, ensure_dir, write_json,
-    RunPaths, ReportWriter, emit_polygon_bounds_wgs84,
-    emit_file_records, emit_related_urls, write_emit_metadata, 
-    bounds_from_bbox, centroid_from_bounds, pick_s2_assets_minimal, 
-    write_s2_metadata, tif_geo_summary, TileRecord, 
-    write_tile_metadata, write_manifest_csv, 
-    copy_any, write_archive_map, describe_tif
+# from .pairs_artifacts import (
+#     utc_now_iso, ensure_dir, write_json,
+#     RunPaths, ReportWriter, emit_polygon_bounds_wgs84,
+#     emit_file_records, emit_related_urls, write_emit_metadata, 
+#     bounds_from_bbox, centroid_from_bounds, pick_s2_assets_minimal, 
+#     write_s2_metadata, tif_geo_summary, TileRecord, 
+#     write_tile_metadata, write_manifest_csv, 
+#     copy_any, write_archive_map, describe_tif
 
-)
+# )
 
-__all__ = [
-    'utc_now_iso', 'ensure_dir', 'write_json',
-    'RunPaths', 'ReportWriter', 'emit_polygon_bounds_wgs84',
-    'emit_file_records', 'emit_related_urls', 'write_emit_metadata', 
-    'bounds_from_bbox', 'centroid_from_bounds', 'pick_s2_assets_minimal', 
-    'write_s2_metadata', 'tif_geo_summary', 'TileRecord', 
-    'write_tile_metadata', 'write_manifest_csv', 
-    'copy_any', 'write_archive_map', 'describe_tif'
-]
+# __all__ = [
+#     'utc_now_iso', 'ensure_dir', 'write_json',
+#     'RunPaths', 'ReportWriter', 'emit_polygon_bounds_wgs84',
+#     'emit_file_records', 'emit_related_urls', 'write_emit_metadata', 
+#     'bounds_from_bbox', 'centroid_from_bounds', 'pick_s2_assets_minimal', 
+#     'write_s2_metadata', 'tif_geo_summary', 'TileRecord', 
+#     'write_tile_metadata', 'write_manifest_csv', 
+#     'copy_any', 'write_archive_map', 'describe_tif'
+# ]
